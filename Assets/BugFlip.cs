21abbaf2b39c484db929455b5b604263e1cb5dbf
Scott Bromander <scott@primeacademy.io>
2016-06-27 20:40:21 -0500

    initial commit

---
+++ Assets/BugFlip.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BugFlip : MonoBehaviour {
+	private bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && facingRight) {
+			Flip ();
+		} 
+		if (Input.GetKeyDown (KeyCode.RightArrow) && !facingRight) {
+			Flip ();
+		}
+	}
+
+	void Flip(){
+		facingRight = !facingRight;
+
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
